fbd2afcf5fcb5e32cab705bfbc2d97b474cb0351
DEV01ASM <erazo@agristar.com.mx>
2024-01-08 19:29:11 -0600

    Object.defineProperty 4/20

--- main.js
+++ main.js
@@ -13,19 +13,32 @@ const juan = {
 //Muestra en un array solo los nombres de las keys del objeto
 //console.log(Object.keys(juan));
 
-//Muestra en un array solo los nombres de las keys del objeto
+//Muestra en un array solo los nombres de las keys del objeto, muestra todas aunque su propiedad enumerable este en false
 //console.log(Object.getOwnPropertyNames(juan));
 
 //muestra en un array de arrays los atributos y valores de un objeto
 //console.log(Object.entries(juan));
 
 //muestra en un objeto de objetos los atributos y valores de un objeto
-console.log(Object.getOwnPropertyDescriptors(juan));
+
 
 //Para asignar una nueva propiedad
 //Recibe 3 argumentos = objeto + nombreNuevaPropiedad + objeto con atributos
-Object.defineProperty(juan, "pruebaNASA", {
-    value: "extraterrestres", 
-    enumerable: true, 
-    writable: true, 
-    configurable: true});
+
+/* Object.defineProperty(juan, "navigator", {
+    value: "Chrome", 
+    enumerable: false, //permite o evita que se visualize
+    writable: true, //permite o evita que se edite   
+    configurable: true //permite o evita que lo borren
+}); */
+
+//NUEVOS METODOS del superprototipo Object Freeze  y Seal
+
+//pone CONFIGURABLE en false en cada propiedad del objeto  
+//Object.seal(juan);
+
+//pone CONFIGURABLE y WRITABLE en false en cada propiedad del objeto 
+Object.freeze(juan);
+
+
+console.log(Object.getOwnPropertyDescriptors(juan));
